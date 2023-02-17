72094fd38935e6d76dd106a36a8d71bbeb8597d7
gogo <2311129161@qq.com>
2023-02-17 11:56:06 +0800

    测试提交

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>这是一个配置了vuex和vue-router的vue3项目a</h1>
+</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+// 导入vue-router
+import { createRouter, createWebHashHistory } from 'vue-router'
+export default createRouter({
+  // 指定路由模式
+  history: createWebHashHistory(),
+  // 路由地址
+    routes:[]
+})
